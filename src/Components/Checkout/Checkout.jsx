eff62320fa1c13feba04f1f8310b2e595dc30165
miyad <1505074.msr@ugrad.cse.buet.ac.bd>
2021-09-15 15:04:11 +0600

    checkout added

--- src/Components/Checkout/Checkout.jsx
+++ src/Components/Checkout/Checkout.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
 
 const Checkout = () => {
     return (
         <div className="items-center">
-            Your order is placed for checkout
+            Your order is placed for checkout <br />
+            <Link to="/">
+                <button>Go to home</button>
+            </Link>
         </div>
     );
 }
